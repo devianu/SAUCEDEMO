e85ae4facafa081b4455b8d87ab09c2e8cde2cd4
Anuradha <anuradhadevireddy3@gmail.com>
2026-02-28 18:42:37 +0530

    Testing Jenkins change detection

--- src/test/java/TEST/LoginTest.java
+++ src/test/java/TEST/LoginTest.java
@@ -1,17 +1,16 @@
 package TEST;
 
 import org.testng.annotations.Test;
-
-
 import BASE.BaseTest;
 import PAGES.LoginPage;
 
 public class LoginTest extends BaseTest {
 
     @Test
-    public void loginTest()
-    {
+    public void loginTest() {
+
         LoginPage login = new LoginPage(driver);
         login.login("standard_user", "secret_sauce");
+        System.out.println("Jenkins change detection test");
     }
 }
